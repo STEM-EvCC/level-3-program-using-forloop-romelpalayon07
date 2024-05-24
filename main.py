a2ccaea336a0bbdddd4ab77a26a619ceefef3e2a
romelpalayon07 <165983784+romelpalayon07@users.noreply.github.com>
2024-05-24 00:24:28 +0000

    finished.

--- main.py
+++ main.py
@@ -5,6 +5,26 @@ mission_success = [True, False, True, True, True, True, False]
 #prints out how many missions there are
 countOfMission = 0
 for missions in mission_success:
-    countOfMission =+ 1
-
+    countOfMission = countOfMission + 1
+print()
 print("Total number of missions: " + str(countOfMission))
+
+#prints out how many of the missions were successful
+successfulMissionsCount = 0
+for successfulMission in mission_success:
+    if successfulMission == True:
+        successfulMissionsCount += 1
+
+print("Number of successful missions: " + str(successfulMissionsCount))
+
+#prints out the success rate
+successRate = (successfulMissionsCount/countOfMission)*100
+
+print(f"Success rate: {successRate:.2f}%")
+
+#prints out which of the missions were launched before 2000
+print("Missions launched before the year 2000:")
+for missionName, missionsBefore2000 in zip (mission_names, mission_years):
+    if missionsBefore2000 < 2000:
+        print("- " + missionName)
+print()
